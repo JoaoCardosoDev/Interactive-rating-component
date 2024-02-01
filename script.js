8ce05471dfa4633f7a71b7a140744df7e6ea21b5
JoaoCardosoDev <jmcardoso90@gmail.com>
2024-02-01 21:57:25 +0000

    Feat: Selection Logic Completed

--- script.js
+++ script.js
@@ -7,27 +7,38 @@ window.onload = () => {
     const sel5 = document.getElementById("5");
     const submit = document.getElementById("submit");
     let res = 0;
+    let list = [sel1, sel2, sel3, sel4, sel5]
 
 sel1.onclick = () => { 
     res = 1;
+    resetBtn(list);
+    checked(sel1);
 }
 sel2.onclick = () => {
     res = 2;
+    resetBtn(list);
+    checked(sel2);
 }
 sel3.onclick = () => {
     res = 3;
+    resetBtn(list);
+    checked(sel3);
 }
 sel4.onclick = () => {
     res = 4;
+    resetBtn(list);
+    checked(sel4);
 }
 sel5.onclick = () => {
     res = 5;
+    resetBtn(list);
+    checked(sel5);
 }
 
 submit.onclick = () => {
     switch (true) {
         case res === 1:
-            console.log("1")
+            thankYou(res);
             break;
         case res === 2:
             console.log("2")
@@ -46,4 +57,30 @@ submit.onclick = () => {
             break;
     }
 }
+}
+
+
+function resetBtn(list){
+    list.forEach(element => {
+        if (element.classList.contains('activeBtn'));
+            element.classList.remove('activeBtn');
+            element.classList.add('secButton'); 
+    });
+
+}
+function checked(button) {
+    button.classList.remove('secButton')
+    button.classList.add('activeBtn')
+}
+
+function thankYou(res) {
+    let img = document.getElementsByTagName("img");
+    let text = document.getElementsByClassName("title");
+    let title = document.getElementsByTagName("h1")
+    img.src = "images/illustration-thank-you";
+    text.innerText = "We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!";
+    title.innerText = "Thank you!";
+    console.log("smth wrong")
+
+
 }
